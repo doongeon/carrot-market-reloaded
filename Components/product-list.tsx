4df18434c71c5aa819caff0778d68380d7754c1b
donggeon Na <ndmb2012@gmail.com>
2024-05-16 21:36:06 +0900

    learn: intersection observer

--- Components/product-list.tsx
+++ Components/product-list.tsx
@@ -5,6 +5,7 @@ import { formatToTimeAge, formatToWon } from "@/libs/utils";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
+import { isLatLong } from "validator";
 
 interface Product {
   id: number;
@@ -21,15 +22,42 @@ interface ProductListProps {
 }
 
 export default function ProductList({ initialProducts }: ProductListProps) {
-  const trigger = useRef(null);
+  const trigger = useRef<HTMLButtonElement>(null);
+  const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(1);
   const [pageEnd, setPageEnd] = useState(false);
   const [products, setProducts] = useState<Product[]>(initialProducts);
 
-  IntersectionObserver
-
   useEffect(() => {
-    console.log(page);
+    const observer = new IntersectionObserver(
+      async (
+        entries: IntersectionObserverEntry[],
+        oberser: IntersectionObserver
+      ) => {
+        const element = entries[0];
+
+        if (element.isIntersecting && trigger.current) {
+          setLoading(true);
+          const moreProducts = await getMoreProducts(page);
+
+          if (moreProducts.length !== 0) {
+            setProducts((prev) => [...prev, ...moreProducts]);
+            setPage((prev) => prev + 1);
+          } else {
+            setPageEnd(true);
+          }
+
+          setLoading(false);
+        }
+      },
+      { threshold: 1.0, rootMargin: "-80px 0%" }
+    );
+
+    if (observer && trigger.current) {
+      observer.observe(trigger.current);
+    }
+
+    return () => observer.disconnect();
   }, [page]);
 
   const handleMore = async () => {
@@ -57,7 +85,7 @@ export default function ProductList({ initialProducts }: ProductListProps) {
               href={`/products/${product.id}`}
               className="w-full flex gap-5 items-center bg-neutral-800 rounded-md  py-3 px-3 hover:opacity-80 "
             >
-              <div className="size-max rounded-md">
+              <div className="w-40 aspect-square rounded-md flex items-center justify-center overflow-hidden">
                 <Image
                   alt="product img"
                   src={product.photo}
@@ -78,13 +106,17 @@ export default function ProductList({ initialProducts }: ProductListProps) {
           );
         })}
       </div>
-      {pageEnd ? <div className="text-neutral-600">다 봤어요...</div>: (
+      {pageEnd ? (
+        <div>다 봤어요...</div>
+      ) : (
         <button
           ref={trigger}
           onClick={handleMore}
-          className="primary-btn w-1/2 max-w-screen-sm rounded-md text-sm font-normal"
+          className={`${
+            loading ? "disable-btn" : "primary-btn"
+          } w-1/2 max-w-screen-sm rounded-md text-sm font-normal`}
         >
-          더 불러오기
+          {loading ? "로딩" : "더 불러오기"}
         </button>
       )}
     </div>
--- app/(tabs)/products/action.ts
+++ app/(tabs)/products/action.ts
@@ -1,10 +1,13 @@
-"use server"
+"use server";
 
 import db from "@/libs/db";
 import { NUM_ITEMS_ON_PAGE } from "./page";
 
 export const getMoreProducts = async (page: number) => {
   return await db.product.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
     skip: NUM_ITEMS_ON_PAGE * page,
     take: NUM_ITEMS_ON_PAGE,
   });
