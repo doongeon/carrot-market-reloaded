0869194746f2e8326d2f478b61b7fc4b1fb6b57a
donggeon Na <ndmb2012@gmail.com>
2024-05-21 10:49:18 +0900

    docs: update gitignore

--- libs/secrets.ts
+++ libs/secrets.ts
@@ -1,4 +1,4 @@
 export const albumBucketName = "bg3-dict";
 export const bucketRegion = "ap-northeast-2";
-export const IdentityPoolId = "ap-northeast-2:86ce3a86-7ef0-460b-aeb0-12cb4c52e6af";
+export const IdentityPoolId = "ap-northeast-2:fca7c2ca-571e-4306-a1c7-c35e68b3003f";
 export const S3_URL = "https://bg3-dict.s3.ap-northeast-2.amazonaws.com"
